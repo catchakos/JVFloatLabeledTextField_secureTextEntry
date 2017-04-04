d0a0762334fcc89f36074b744ee684f874c79fe8
Alexis Katsaprakakis <alexis.katsaprakakis@inqbarna.com>
2017-04-04 11:44:30 +0200

    Initial Commit

---
+++ testFloatingTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFloatingTextField
+//
+//  Created by Alexis Katsaprakakis on 04/04/17.
+//  Copyright © 2017 Alexis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFloatingTextField/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testFloatingTextField
+//
+//  Created by Alexis Katsaprakakis on 04/04/17.
+//  Copyright © 2017 Alexis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
